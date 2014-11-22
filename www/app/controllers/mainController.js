1eeb93a998e65d5f44f4cd19585fd2cbb7308f7c
ByteBlast <byteblast@gmail.com>
2014-11-22 22:15:30 +0000

    Simple theme store infrastructure.

--- www/app/controllers/mainController.js
+++ www/app/controllers/mainController.js
@@ -1,3 +1,10 @@
-app.controller('mainController', function($scope) {
-   $scope.message = "Hello"; 
+app.controller('mainController', function($scope, $http) {
+
+    function themesAvailable(themes) {
+        $scope.themes = themes;
+    };
+
+    $http.get('app/db/themes.json')
+        .success(themesAvailable);
+
 });
