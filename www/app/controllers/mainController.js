c7d11dfd5f5fdf60a6129d95632a6f266368b15f
szdc <jack.willis-craig@hotmail.com>
2014-11-24 16:53:05 +1100

    Added a generic HTTP error handling method.

--- www/app/controllers/mainController.js
+++ www/app/controllers/mainController.js
@@ -3,7 +3,12 @@ app.controller('mainController', function($scope, $http) {
     function themesAvailable(themes) {
         $scope.themes = themes;
     };
+    
+    function httpError(data, status, headers, config) {
+        console.log('HTTP Error ' + status + ': ' + data);
+    }
 
     $http.get('app/db/themes.json')
-        .success(themesAvailable);
+        .success(themesAvailable)
+        .error(httpError);
 });
